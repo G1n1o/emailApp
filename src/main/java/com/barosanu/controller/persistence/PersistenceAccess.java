1e6c515e9e5f5caa1509531f343b4cdf1584c16b
G1n1o <rafal.szkudlarek88@gmail.com>
2024-10-28 05:51:05 +0100

    finished lessons

--- src/main/java/com/barosanu/controller/persistence/PersistenceAccess.java
+++ src/main/java/com/barosanu/controller/persistence/PersistenceAccess.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class PersistenceAccess {
 
     private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "validAccounts.ser";
+    private Encoder encoder = new Encoder();
 
     public List<ValidAccount> loadFromPersistence(){
         List<ValidAccount> resultList = new ArrayList<ValidAccount>();
@@ -15,11 +16,25 @@ public class PersistenceAccess {
             FileInputStream fileInputStream = new FileInputStream(VALID_ACCOUNTS_LOCATION);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+            decodePasswords(persistedList);
             resultList.addAll(persistedList);
         } catch ( Exception e){
             e.printStackTrace();
         }
         return resultList;
+    }
+
+    private void decodePasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.decode(originalPassword));
+        }
+    }
+    private void encodePasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.encode(originalPassword));
+        }
     }
 
 
@@ -31,6 +46,7 @@ public class PersistenceAccess {
         try {
             fileOutputStream = new FileOutputStream(file);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            encodePasswords(validAccounts);
             objectOutputStream.writeObject(validAccounts);
         } catch (IOException e) {
             e.printStackTrace();
